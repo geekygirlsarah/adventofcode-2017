508b53ad87e7e3519984a549c3132ecf40fdc607
Sarah Withee <github@geekygirlsarah.com>
2017-12-12 00:21:07 -0600

    Add Day 9 solutions and test input

---
+++ day09/day09part2.py
@@ -0,0 +1,65 @@
+# Advent of Code 2017
+# Day 9, Part 2
+# @geekygirlsarah
+
+# Files to run through
+# input.txt being the input the puzzle provides
+inputFile = "input.txt"
+# inputFile = "testinput.txt"
+
+
+# Process file
+with open(inputFile) as f:
+    while True:
+        contents = f.readline(-1)
+        if not contents:
+            # print "End of file"
+            break
+        # print ("Contents: ", contents)
+
+        state = "start"
+        group_level = 0
+        group_sum = 0
+        char_pos = -1
+        garbage_count = 0
+
+        while True:
+            # increment char
+            char_pos += 1
+            if state == "end" or char_pos >= len(contents):
+                break
+
+            char = contents[char_pos]
+
+            # Starting state
+            if state == "start" and char == "{":
+                group_level += 1
+                state = "in group"
+                continue
+            if state == "in group" and char == "{":
+                group_level += 1
+                continue
+            if state == "in group" and char == "}":
+                group_sum += group_level
+                group_level -= 1
+                if group_level == 0:
+                    state = "end"
+                continue
+            if state == "in group" and char == "<":
+                state = "garbage"
+                continue
+            if state == "garbage" and char == ">":
+                state = "in group"
+                continue
+            if state == "garbage" and char == "!":
+                char_pos += 1
+                continue
+            if state == "garbage" and char == ">":
+                state = "in group"
+                continue
+            if state == "garbage":    # catch misc. chars
+                garbage_count += 1
+
+        print("Sum: " + str(group_sum))
+        print("Garbage count: " + str(garbage_count) + "\n")
+
